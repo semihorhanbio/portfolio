ddd84511d545bbc420d176083a9507ce4a2764fc
Semih Orhan <sorhan3.14@gmail.com>
2024-01-24 08:39:30 +0300

    create about me

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
+
 const NavLink = ({ href, title }) => {
   return (
     <Link
       href={href}
-      class="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:bg-transparent  md:p-0 hover:text-white "
-      aria-current="page"
+      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
     >
       {title}
     </Link>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,11 @@
 "use client";
-import React, { useState } from "react";
 import Link from "next/link";
+import React, { useState } from "react";
 import NavLink from "./NavLink";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XmarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
-  {
-    title: "Home",
-    path: "/",
-  },
   {
     title: "About",
     path: "#about",
@@ -28,46 +24,38 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
-          href="/"
+          href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
           LOGO
         </Link>
-        <div className="block md:hidden">
+        <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
             <button
-              id="nav-toggle"
-              className="flex items-center px-3 py-2 border rounded text-slate-200 border-slate-200 hover:text-white hover:border-white"
               onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
               <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
             <button
-              id="nav-toggle"
-              className="flex items-center px-3 py-2 border rounded text-slate-200 border-slate-200 hover:text-white hover:border-white"
               onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
-              <XMarkIcon className="h-5 w-5" />
+              <XmarkIcon className="h-5 w-5" />
             </button>
           )}
         </div>
-        <div className="hidden md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex p-4 md:p-0  rounded-lg flex-row md:space-x-8 mt-0">
-            {navLinks.map((link) => {
-              return (
-                <li>
-                  <NavLink
-                    key={link.title}
-                    title={link.title}
-                    href={link.path}
-                  />
-                </li>
-              );
-            })}
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
